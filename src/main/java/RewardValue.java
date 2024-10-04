306a2ff9944a0029c78efc1ed55fdbf5d653c8da
Umar Imtiaz <umarimtiaz720@gmail.com>
2024-10-04 06:06:37 +0100

    Added RewardValue class with cash and miles constructors

---
+++ src/main/java/RewardValue.java
@@ -0,0 +1,26 @@
+public class RewardValue {
+    private double cashValue;
+    private int milesValue;
+
+    // Constructor that accepts a cash value
+    public RewardValue(double cashValue) {
+        this.cashValue = cashValue;
+        this.milesValue = (int) (cashValue * 1000);
+    }
+
+    // Constructor that accepts a miles value
+    public RewardValue(int milesValue) {
+        this.milesValue = milesValue;
+        this.cashValue = milesValue * 0.0035;
+    }
+
+    // Method to get the cash value
+    public double getCashValue() {
+        return cashValue;
+    }
+
+    // Method to get the miles value
+    public int getMilesValue() {
+        return milesValue;
+    }
+}
